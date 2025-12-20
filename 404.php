ebfa6cb4a37b709bcf0a039ecdda7502f8a27b5b
René Jossen <rene@enjor.ch>
2025-12-20 23:24:46 +0100

    Refactor 404 error handling; include header and page template logic for better structure

--- 404.php
+++ 404.php
@@ -1,18 +1,26 @@
-<?php get_header();?>
+<?php 
 
-<?php
 	$args = array(
 		'name'        => 'error-404',
 		'post_type'   => 'page',
 		'post_status' => 'publish',
 		'numberposts' => 1
 	);
+	
 	$page_404 = get_posts($args);
 	if ($page_404) {
-		$content = $page_404[0]->post_content;
-		$content = apply_filters('the_content', $content);
-		echo $content;
+		$post = $page_404[0];
+		setup_postdata( $post );
+		
+		if (file_exists(get_template_directory() . '/page.php')) {
+			include( get_template_directory() . '/page.php' );
+		} else {
+			include( get_template_directory() . '/index.php' );
+		}		
+		
+		wp_reset_postdata();
+	} else {
+		header("HTTP/1.0 404 Not Found");
+		header("Location: " . home_url());
+		exit();
 	}
-?>
-
-<?php get_header();?>
